46852695ff4eab359e7e727a9216679a78765444
hwhinowa <jejinoes1@gmail.com>
2019-06-13 18:12:45 +0900

    commit

---
+++ js/main.js
@@ -0,0 +1,122 @@
+'use strict';
+
+document.addEventListener('DOMContentLoaded', function(){
+    // 페이지 로드완료시 시작점으로 스크롤 이동
+    setTimeout(function(){
+        window.scrollTo(0,0);
+    },2000);
+});
+
+// 선택자 객체 지정 함수
+function $(selector){
+	let $el = document.querySelectorAll(selector),
+		count = $el.length;
+
+	if(count > 1){
+		return $el;
+	}else if(count == 1){
+		return $el[0];
+	}else{
+		return;
+	}
+};
+
+// WORK메뉴 버튼 클릭 이벤트
+const work=()=>{
+    let body = $('body');
+    let $box = $('.work_empty');
+    let $wrap = $('.wrap');
+
+    $box.classList.add('active');
+
+    setTimeout(function(){
+        $wrap.classList.add('active');
+        body.classList.add('active');
+    }, 500);
+    setTimeout(function(){
+        location.href = 'E:/Desktop/codes/portfolio/html/works.html';
+    }, 1500);
+}
+
+// 메뉴 마우스오버 이벤트
+let $menu = $('.menu');
+
+const back_modify=(event)=>{
+    let box, elem, style;
+
+    box = event.target.classList.contains('menu') ? event.target : event.target.closest('button');
+    elem = box.classList.contains('about') ? 'left' : 'right';
+
+    if(event.type === 'mouseover'){
+        style = true;
+    }else if(event.type === 'mouseout'){
+        style = false;
+    }
+
+    style_active(elem, style);
+};
+
+const style_active=(back, active)=>{
+    let $target = $('.bg.'+back);
+    if(active){
+        $target.classList.add('hover');
+    }else{
+        $target.classList.remove('hover');
+    }
+};
+
+for(let i = 0; i < $menu.length; i++){
+    let temp = $menu[i];
+    temp.onmouseover = temp.onmouseout = back_modify;
+}
+
+// 스크롤 이벤트
+window.addEventListener('scroll', function(e){
+    let scroll_y = window.scrollY;
+    let $target_bg = $('.bg');
+    let $target_hello = $('.text_box');
+    let $target_text = $('.text');
+
+    for(let i = 0; i < $target_bg.length; i++){
+        let temp = $target_bg[i];
+        if(scroll_y >= 100 && $target_bg.length !== 0){
+            temp.classList.add('con');
+            $target_hello.classList.add('con');
+            $target_text.classList.add('show');
+        }else{
+            temp.classList.remove('con');
+            $target_hello.classList.remove('con');
+            $target_text.classList.remove('show');
+        }
+    }
+});
+
+// 소개페이지 패럴랙스 스크롤
+let $about = $('.about_container');
+$about.addEventListener('scroll', function(){
+    let about_test = $about.scrollTop,
+        width = (window.innerWidth || document.documentElement.clientWidth),
+        $box = $('.two');
+    
+    if(about_test > 0 && about_test < 500){
+        if(width > 1000){
+            $box.style.top = (400 - about_test / 2) + 'px';
+        }else{
+            $box.style.top = (288 - about_test / 2) + 'px';
+        }
+    }
+});
+
+// ABOUT 메뉴 OPEN/CLOSE
+let $elem = $('main');
+let $header = $('header');
+
+const menu_open=()=>{
+    $elem.classList.add('menu');
+    $header.classList.add('menu');
+}
+
+const menu_close=()=>{
+    $elem.classList.remove('menu');
+    $header.classList.remove('menu');
+}
---
+++ js/works.js
@@ -0,0 +1,158 @@
+'use strict';
+
+// 선택자 객체 지정 함수
+function $(selector){
+	let $el = document.querySelectorAll(selector),
+		count = $el.length;
+
+	if(count > 1){
+		return $el;
+	}else if(count == 1){
+		return $el[0];
+	}else{
+		return;
+	}
+};
+
+// 모든 classname 삭제
+const remove_all_class = (elem) => {
+    if (elem && elem.classList) {
+        for (let i = 0; i < elem.classList.length; i++) {
+            let temp = elem.classList[i];
+            elem.classList.remove(temp);
+        }
+    }
+}
+
+// 스크롤바 스타일 변경 함수
+const move=(e)=>{
+    let $box = $('#well'),
+        $buttons = $('.fix_box3 > button'),
+        $bar = $('.scroll_view span:nth-of-type(8)');
+
+    for(let i = 0; i <= $buttons.length; i++){
+        let temp = $buttons[i];
+        if(temp === e){
+            $box.style.transform = 'translateY(' + (i * (-100)) + 'vh)';
+            remove_all_class($bar);
+            let will_add_class = 'item' + (i + 1);
+            if (i !== 0) {
+                $bar.classList.add(will_add_class);
+            }
+        }
+    }
+}
+
+// class 토글 함수
+const toggle_class=(elem, name)=>{
+    if(elem && elem.classList){
+        if(elem.classList.contains(name)){
+            elem.classList.remove(name);
+        }else{
+            elem.classList.add(name);
+        }
+    }
+}
+
+// 인덱스 OPEN/CLOSE
+const view_index=(btn, mobile)=>{
+    let mobile_check = mobile === undefined ? false : mobile,
+        $util_box = $('.introduce_box .util'),
+        $box = $('.fix_box3');
+
+    toggle_class($box, 'active');
+    if(mobile_check){
+        let btns = $('.introduce_box .util button:nth-of-type(2)');
+        for(let i = 0; i < btns.length; i++){
+            let temp = btns[i];
+            toggle_class(temp, 'active');
+            if(temp && temp.classList){
+                if(temp.classList.contains('active')){
+                    temp.innerHTML = 'Close';
+                    $util_box.forEach(function(elem){
+                        elem.classList.add('active');
+                    });
+                }else{
+                    temp.innerHTML = 'Index';
+                    $util_box.forEach(function(elem){
+                        elem.classList.remove('active');
+                    });
+                }
+            }
+        }
+    }else{
+        toggle_class(btn, 'active');
+        if(btn && btn.classList){
+            if(btn.classList.contains('active')){
+                btn.innerHTML = 'Close';
+            }else{
+                btn.innerHTML = 'Index';
+            }
+        }
+    }
+}
+
+// fullpage 스크롤 이동 함수
+let scroll_direction, hold = false,
+    panels = $('.panel').length;
+
+function _scrollY(obj) {
+    let slength, plength, pan,
+        step = 100,
+        vh = window.innerHeight / 100,
+        vmin = Math.min(window.innerHeight, window.innerWidth) / 100;
+    if ((this !== undefined && this.id === 'well') || (obj !== undefined && obj.id === 'well')) {
+        pan = this || obj;
+        plength = parseInt(pan.offsetHeight / vh);
+    }
+    if (pan === undefined) {
+        return;
+    }
+    plength = plength || parseInt(pan.offsetHeight / vmin);
+    slength = parseInt(pan.style.transform.replace('translateY(', ''));
+    if (scroll_direction === 'up' && Math.abs(slength) < (plength - plength / panels)) {
+        slength = slength - step;
+    } else if (scroll_direction === 'down' && slength < 0) {
+        slength = slength + step;
+    } else if (scroll_direction === 'top') {
+        slength = 0;
+    }
+
+    if (hold === false) {
+        hold = true;
+        pan.style.transform = 'translateY(' + slength + 'vh)';
+
+        let $bar = $('.scroll_view span:nth-of-type(8)');
+
+        remove_all_class($bar);
+        let will_add_class = 'item' + ((slength * (-1) / 100) + 1);
+        if (slength !== 0) {
+            $bar.classList.add(will_add_class);
+        }
+
+        setTimeout(function () {
+            hold = false;
+        }, 1000);
+    }
+}
+
+let well = document.getElementById('well');
+well.style.transform = 'translateY(0)';
+well.addEventListener('wheel', function (e) {
+    if (e.deltaY < 0) {
+        scroll_direction = 'down';
+    }
+    if (e.deltaY > 0) {
+        scroll_direction = 'up';
+    }
+    e.stopPropagation();
+});
+well.addEventListener('wheel', _scrollY);
+
+let $tops = $('.introduce_top');
+for (let i = 0; i < $tops.length; i++) {
+    $tops[i].addEventListener('click', function () {
+        scroll_direction = 'top';
+        _scrollY(well);
+    });
+}
